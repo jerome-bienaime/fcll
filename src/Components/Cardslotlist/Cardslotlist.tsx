f7b0d2bbc8f0c225691c7eb8e3db8c22cc094b13
jerome <jerome.bienaime@gmail.com>
2021-02-16 11:33:38 +0100

    Update Cardslotlist.tsx
    Using Prettier, clean-up code

--- src/Components/Cardslotlist/Cardslotlist.tsx
+++ src/Components/Cardslotlist/Cardslotlist.tsx
@@ -1,34 +1,38 @@
-import React from "react"
-import type { Cardslot } from "@components/Cardslot"
-import _ from "lodash"
-import "./cardslotlist.css"
+import React from 'react';
+import type { Cardslot } from '@components/Cardslot';
+import _ from 'lodash';
+import './cardslotlist.css';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 export enum ViewType {
-    STACK, COLUMN
+  STACK,
+  COLUMN,
 }
 
 export type CardProps = {
-    viewType?: ViewType,
-    children: Cardslot[]
-}
+  viewType?: ViewType;
+  children: Cardslot[];
+};
 
-const Cardslotlist: React.FC<CardProps> = ({children, viewType = ViewType.STACK}) => {
+const Cardslotlist: React.FC<CardProps> = ({
+  children,
+  viewType = ViewType.STACK,
+}) => {
+  const viewTypeAsString = ViewType[viewType].toString().toLowerCase();
 
-    const viewTypeAsString = ViewType[viewType].toString().toLowerCase()
+  if (!children) {
+    return <></>;
+  }
 
-    if (!children) {
-        return <></>
-    }
-    const containerClassName = `container cardslotlist ${viewTypeAsString}`
-    return <div className={containerClassName}>
-        {React.Children.map(children, (child, index) => {
-            const itemClassName = `item`
-            return <div className={itemClassName}>
-                {React.cloneElement(child)}
-            </div>
-        })}
-        
-        </div>
-}
+  const containerClassName = `container cardslotlist ${viewTypeAsString}`;
+  return (
+    <div className={containerClassName}>
+      {React.Children.map(children, (child, index) => {
+        const itemClassName = `item`;
+        return <div className={itemClassName}>{React.cloneElement(child)}</div>;
+      })}
+    </div>
+  );
+};
 
-export {Cardslotlist}
+export { Cardslotlist };
